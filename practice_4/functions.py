98b49d7f26a8a501d28670b5c94b837c0d796117
Emily Bacon <111798698+elle-bacon@users.noreply.github.com>
2023-04-25 23:20:55 -0700

    commiting

---
+++ practice_4/functions.py
@@ -0,0 +1,10 @@
+list = [56,42,9,200,0]
+
+
+function max_num() = print(max(list))
+
+function mult_list() = print(numpy.prod(list))
+
+function rev_string() = print(list ::-1)
+
+functon num_within() = print(random.randint(list))
